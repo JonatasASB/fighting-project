38095355d864743f5cab4766527d8a8968114fa5
jonatas <jonatasabnersb@gmail.com>
2025-01-28 21:19:19 -0300

    adding fighter classes and scenario

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+let char = new Sorceres('Jonatas')
+
+let monster = new LittleMonster();
+
+const stage = new Stage(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+)
+stage.start();
